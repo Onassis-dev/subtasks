8f9797fe55155e0f1f477b0928d8b4b57cfd3876
Onassis <onassis.dev@gmail.com>
2025-10-30 20:37:06 -0700

    feat: add pwa manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,11 @@ import "@/app/globals.css";
 
 export const metadata: Metadata = {
   title: "Subtasks",
+  icons: {
+    icon: "/favicon/favicon.svg",
+    shortcut: "/favicon/favicon.svg",
+    apple: "/favicon/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
